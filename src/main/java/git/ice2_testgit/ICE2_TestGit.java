f04c98b51f0eeca3e6d02a17331ec317cee4f635
leblaluc <lucasleblanc30@gmail.com>
2025-10-02 09:41:34 -0400

    rev 3 local

--- src/main/java/git/ice2_testgit/ICE2_TestGit.java
+++ src/main/java/git/ice2_testgit/ICE2_TestGit.java
@@ -11,6 +11,6 @@ package git.ice2_testgit;
 public class ICE2_TestGit {
 
     public static void main(String[] args) {
-        System.out.println("rev 2 local");
+        System.out.println("rev 3 local");
     }
 }
