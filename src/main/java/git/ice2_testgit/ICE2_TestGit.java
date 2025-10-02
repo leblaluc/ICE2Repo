ee26c43e6d46bbb4b1f8a775558b9c26555fc966
leblaluc <lucasleblanc30@gmail.com>
2025-10-02 09:40:35 -0400

    rev 1 local

---
+++ src/main/java/git/ice2_testgit/ICE2_TestGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package git.ice2_testgit;
+
+/**
+ *
+ * @author lucas
+ */
+public class ICE2_TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("rev 1 local");
+    }
+}
